8d22152b563175d63bbb21903ae9c7ec05aeb77e
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 01:28:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
   # code goes here
+  @@all=[]
+  @@count=0
+  attr_reader :name, :species
+  def initialize(name,species="human")
+    @name =name
+    @species =species
+    @@all << self
+  end
+  
+  def say_species
+     "I am a human."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all 
+    self.all.clear
+  end
+  
+  def cats 
+    
+  
 end
+
