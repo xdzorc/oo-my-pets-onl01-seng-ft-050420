35210d17dce70b8c8e6c49be334a57803dd61a9e
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 02:32:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,7 +66,7 @@ class Owner
   end
   
   def list_pets
-   "I have #{self.dogs.count}dog(s), and #{self.cats.count} cat(s)."
+   "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
   
 end
